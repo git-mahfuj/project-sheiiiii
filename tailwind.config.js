df4880da5cc379809955ee1e1a5444c6fa5ed882
Mahfuj <mahfujurrheeck@.gmail.com>
2025-04-15 16:41:51 +0600

    website

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/assets/background-features.5f7a9ac9.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
